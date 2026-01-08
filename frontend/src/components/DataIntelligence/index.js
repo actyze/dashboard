8d998b28a366203f789d24c13a003d1c80ef0344
Uddish Verma <uddishverma@Uddishs-MacBook-Air.local>
2026-01-08 21:17:10 +0400

    refactor data intelligence

--- frontend/src/components/DataIntelligence/index.js
+++ frontend/src/components/DataIntelligence/index.js
@@ -1,4 +1,5 @@
 export { default as DataIntelligence } from './DataIntelligence';
 export { default as FileImports } from './FileImports';
 export { default as MetadataCatalog } from './MetadataCatalog';
+export { default as SchemaOptimise } from './SchemaOptimise';
 
